dcc377a12fd18726d21948aaf556edff2a54d6a7
sbozzo <santiagobozzo95@gmail.com>
2019-02-06 14:48:16 -0300

    scripts

---
+++ AnotherCardGame/Assets/Scripts/MenuSceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneManager : MonoBehaviour
+{
+
+	public Scene actualScene { get; set; }
+	
+	void Start()
+	{
+		actualScene = SceneManager.GetActiveScene();
+	}
+
+	public void goToTrucoScene()
+	{
+		SceneManager.LoadScene("TrucoScene", LoadSceneMode.Single);
+	}
+
+
+}
