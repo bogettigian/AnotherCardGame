02a64ba8a0e087bd0cb93b29620bb7999add8476
BogettiGian <gianbogetti7@hotmail.com>
2019-03-08 20:57:30 -0300

    empezando con photon bolt

---
+++ AnotherCardGame/Assets/Scripts/NetworkCallbacks.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : GlobalEventListener
+{
+    public GameObject Card1;
+    public GameObject Card2;
+    public GameObject Card3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Card1.SetActive(false);
+        Card2.SetActive(false);
+        Card3.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ AnotherCardGame/Assets/Scripts/NetworkMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+using UdpKit;
+using System;
+
+public class NetworkMenu : GlobalEventListener
+{
+    public void StartServer()
+    {
+        BoltLauncher.StartServer();
+        BoltNetwork.LoadScene("NetworkLoad");
+    }
+
+    public void StartClient()
+    {
+        BoltLauncher.StartClient();
+        BoltNetwork.LoadScene("NetworkLoad");
+    }
+
+    public override void BoltStartDone()
+    {
+        if(BoltNetwork.IsServer)
+        {
+            string matchName = Guid.NewGuid().ToString();
+
+            BoltNetwork.SetServerInfo(matchName, null);
+        }
+    }
+}
